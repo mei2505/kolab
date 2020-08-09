44c71f7db76e937c54ef530ad15319d7e856c49c
Kimio Kuramitsu <kkuramitsu@gmail.com>
2020-08-09 19:08:41 +0900

    add judge.py

---
+++ kolab/judge.py
@@ -0,0 +1,64 @@
+def naming(name, ins):
+  if isinstance(ins, tuple) and len(ins) > 1:
+    return f'{name}{repr(ins)}'
+  return f'{name}({repr(ins)})'
+
+
+def testData(f, data):
+  result = f(*data)
+  return result
+
+
+def match(a, b):
+  return a == b
+
+
+def chk(c):
+  return '✅' if c else '❌'
+
+
+class Judge(object):
+  def __init__(self, data):
+    self.judge_data = data
+
+  def judge(self, name, f):
+    results = []
+    c = 0
+    for i, data in enumerate(self.judge_data):
+      case = data['case'] if 'case' in data else naming(name, data['input'])
+      self.showing(i, case)
+      output = data['output']
+      method = f'test{i}'
+      if hasattr(self, method):
+        testFunction = getattr(self, method)
+      else:
+        testFunction = testData
+      try:
+        result = testFunction(f, data.get('input', []))
+      except Exception as e:
+        result = e
+      # data
+      checked = match(result, output)
+      hint = '' if checked else data.get('hint', '')
+      self.show(i, case, result, checked, output)
+      results.append((i, case, result, checked, output, hint))
+      if checked:
+        c += 1
+    self.showall(c, results)
+
+  def showing(self, i, title):
+    pass
+    #print(f'[{i}] {title} => ...')
+
+  def show(self, i, title, result, checked, output):
+    if isinstance(result, Exception):
+      print(f'[{i}] {title} => {chk(checked)} {result.__class__.__name__}')
+      print(' エラー:', result)
+      return
+    print(f'[{i}] {title} => {chk(checked)} {result}')
+    if not checked:
+      print(' 正解:', output)
+      print(' 誤り:', result)
+
+  def showall(self, c, results):
+    print(f'[{c}/{len(results)}] AC')
---
+++ kolab/lec/calc.py
@@ -0,0 +1,66 @@
+import kolab.judge
+
+calc_data = [
+    {
+        'input': ('1'),
+        'output': 1,
+    },
+    {
+        'input': ('123'),
+        'output': 123,
+    },
+    {
+        'input': ('1+2'),
+        'output': 3,
+    },
+    {
+        'input': ('1+2+3'),
+        'output': 6,
+    },
+    {
+        'input': ('1+2+3+4+5+6+7+8+9+0'),
+        'output': 45,
+    },
+    {
+        'input': ('1+2*3'),
+        'output': 7,
+    },
+    {
+        'input': ('1*2+3'),
+        'output': 5,
+    },
+    {
+        'input': ('1*2+3*4'),
+        'output': 14,
+    },
+    {
+        'input': ('1*2+3*4+5*6+7*8+9'),
+        'output': 109,
+    },
+    {
+        'input': ('1-2'),
+        'output': -1,
+    },
+    {
+        'input': ('1-2-3'),
+        'output': -4,
+    },
+    {
+        'input': ('7/2'),
+        'output': 3,
+    },
+    {
+        'input': ('1+2*3/4'),
+        'output': -4,
+    },
+    {
+        'input': ('(1+2)*3'),
+        'output': 9,
+    },
+]
+
+def judge(function):
+  oj = Judge(gcd_data)
+  oj.judge('calc', function)
+
+

092fbc2b4f8d74ce805edcfda1f0c303cb36b3aa
Kimio Kuramitsu <kkuramitsu@gmail.com>
2020-08-09 19:50:57 +0900

    fix: cacl.py

--- kolab/lec/calc.py
+++ kolab/lec/calc.py
@@ -2,64 +2,64 @@ from kolab.lec.judge import Judge
 
 judge_data = [
     {
-        'input': ('1',),
+        'input': '1',
         'output': 1,
     },
     {
-        'input': ('123',),
+        'input': '123',
         'output': 123,
     },
     {
-        'input': ('1+2',),
+        'input': '1+2',
         'output': 3,
     },
     {
-        'input': ('1+2+3',),
+        'input': '1+2+3',
         'output': 6,
     },
     {
-        'input': ('1+2+3+4+5+6+7+8+9+0',),
+        'input': '1+2+3+4+5+6+7+8+9+0',
         'output': 45,
     },
     {
-        'input': ('1+2*3',),
+        'input': '1+2*3',
         'output': 7,
     },
     {
-        'input': ('1*2+3',),
+        'input': '1*2+3',
         'output': 5,
     },
     {
-        'input': ('1*2+3*4',),
+        'input': '1*2+3*4',
         'output': 14,
     },
     {
-        'input': ('1*2+3*4+5*6+7*8+9',),
+        'input': '1*2+3*4+5*6+7*8+9',
         'output': 109,
     },
     {
-        'input': ('1-2',),
+        'input': '1-2',
         'output': -1,
     },
     {
-        'input': ('1-2-3',),
+        'input': '1-2-3',
         'output': -4,
     },
     {
-        'input': ('7/2',),
+        'input': '7/2',
         'output': 3,
     },
     {
-        'input': ('1+2*3/4',),
+        'input': '1+2*3/4',
         'output': -4,
     },
     {
-        'input': ('(1+2)*3',),
+        'input': '(1+2)*3',
         'output': 9,
     },
 ]
 
-def judge0(func):
+def judge(func):
   oj = Judge(judge_data)
   oj.judge('calc', func)
 

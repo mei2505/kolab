fe77b429c1b47ba8421451bc15c6402219b59806
Kimio Kuramitsu <kkuramitsu@gmail.com>
2020-09-24 12:31:13 +0900

    kolab/regio.py

--- kolab/lec/gcd.py
+++ kolab/lec/gcd.py
@@ -1,34 +1,12 @@
-
-gcd_data = [
+judge_data = [
     {
-        'input': (6215, 4746),
-        'output': 113,
-    },
-    {
-        'input': (4746, 6215),
-        'output': 113,
-    },
+      'input': (6215, 4746),
+      'output': 113,
+    }
+    
 ]
 
-judge_data = {
-    'gcd': gcd_data,
-}
 
-
-def show(name, ins):
-  if isinstance(ins, tuple) and len(ins) > 1:
-    return f'{name}{repr(ins)}'
-  return f'{name}({repr(ins)})'
-
-
-def match(a, b):
-  return a == b
-
-def judge(name, names = globals()):
-  #names = globals()
-  print(names)
-  f = names[name]
-  for data in judge_data[name]:
-    print(show(name, data['input']), '=>', end='')
-    output = f(*data['input'])
-    print(output, data['output'])
+def judge_gcd(func):
+  oj = Judge(judge_data_gcd)
+  oj.judge('gcd', func)
---
+++ kolab/regio.py
@@ -0,0 +1,26 @@
+from IPython.core.magic import register_cell_magic
+
+original_input = input
+magic_buffer = []
+magic_pos = 0
+
+@register_cell_magic
+def In(lines, src):
+  global magic_buffer
+  magic_buffer = []
+  for line in lines.split('\n'):
+    if len(line) == 0:
+      continue
+    magic_buffer.append(line)
+  return magic_buffer
+
+def input(prompt = ''):
+  global magic_buffer, magic_pos
+  if prompt != '' or len(magic_buffer) == 0:
+    return original_input(prompt)
+  if magic_pos < len(magic_buffer):
+    magic_pos += 1
+    return magic_buffer[magic_pos-1]
+  else:
+    raise EOFError()
+

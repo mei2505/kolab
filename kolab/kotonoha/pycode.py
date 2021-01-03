1dac8437ba9dda8250b9ffac3c2a4c156ba8e8c8
kkuramitsu <kkuramitsu@gmail.com>
2021-01-03 17:43:49 +0900

    pycode

---
+++ kolab/kotonoha/pycode.py
@@ -0,0 +1,373 @@
+import sys
+import pegtree as pg
+from pegtree import ParseTree
+from kolab.kotonoha.visitor import TransCompiler
+
+RESERVED = {
+    'math', 'numpy', 'np', 'pandas', 'pd'
+}
+
+
+class PythonCode(TransCompiler):
+
+    def __init__(self, grammar='../pegtree/puppy2.tpeg'):
+        TransCompiler.__init__(self)
+        peg = pg.grammar(grammar)
+        self.parser = pg.generate(peg)
+        self.rootEnv = {}
+
+    def load(self, modules, experimental=False):
+        pass
+
+    def compile(self, source):
+        tree = self.parser(source)
+        self.indent = 0
+        self.names = {}
+        return self.stringfy(tree, ' ')
+
+    def isIndexedName(self, name):
+        return name not in RESERVED
+
+    def index_name(self, name):
+        if self.isIndexedName(name):
+            if name not in self.names:
+                s = chr(ord('A')+len(self.names))
+                self.names[name] = f'[${s}]'
+            return self.names[name]
+        else:
+            return name
+
+    def acceptSeq(self, tree, sep=','):
+        for i, e in enumerate(tree.getSubNodes()):
+            if i > 0:
+                self.push(sep)
+            self.visit(e)
+
+    def acceptSource(self, tree):
+        trees = tree.getSubNodes()
+        for t in trees:
+            self.pushBOS()
+            self.visit(t)
+
+    def acceptBlock(self, tree):
+        self.indent += 1
+        self.acceptSource(tree)
+        self.indent -= 1
+
+    def acceptDocument(self, tree):
+        pass
+
+    # Statement
+
+    def acceptPass(self, tree):
+        self.push('pass')
+
+    def acceptAssert(self, tree):
+        self.push('assert')
+        self.visit(tree.cond)
+
+    def acceptIf(self, tree):
+        self.push('if')
+        self.visit(tree.cond)
+        self.push(':')
+        self.visit(tree.get('then'))
+        if tree.has('elif'):
+            for t in tree.get('elif').getSubNodes():
+                self.visit(t)
+        if tree.has('else'):
+            self.visit(tree.get('else'))
+
+    def acceptElif(self, tree):
+        self.pushBOS()
+        self.push('elif')
+        self.visit(tree.cond)
+        self.push(':')
+        self.visit(tree.get('then'))
+
+    def acceptElse(self, tree):
+        self.pushBOS()
+        self.push('else')
+        self.push(':')
+        self.visit(tree[0])
+
+    def acceptWhile(self, tree):
+        self.push('while')
+        self.visit(tree.cond)
+        self.push(':')
+        self.visit(tree.body)
+
+    def acceptTry(self, tree):
+        self.push('try')
+        self.push(':')
+        self.visit(tree.body)
+        if tree.has('except'):
+            for e in tree.get('except'):
+                self.visit(e)
+        if tree.has('else'):
+            self.visit(tree.get('else'))
+        if tree.has('finally'):
+            self.visit(tree.get('finally'))
+
+    def acceptExcept(self, tree):
+        self.pushBOS()
+        self.push('except')
+        if tree.has('cond'):
+            self.push(str(tree.cond))
+            if tree.has('as'):
+                self.push('as')
+                self.visit(tree.get('as'))
+        self.push(':')
+        self.visit(tree.body)
+
+    def acceptFinally(self, tree):
+        self.pushBOS()
+        self.push('finally')
+        self.push(':')
+        self.visit(tree[0])
+
+    # break
+
+    def acceptBreak(self, tree):
+        self.push('break')
+
+    # continue
+    def acceptContinue(self, tree):
+        self.push('continue')
+
+    def acceptFor(self, tree):
+        self.push('for')
+        self.acceptSeq(tree.each)
+        self.push('in')
+        self.visit(tree.list)
+        self.push(':')
+        self.visit(tree.body)
+
+    def acceptImportDecl(self, tree):
+        self.push('import')
+        name = str(tree.name)
+        self.push(name)
+
+    def acceptVarDecl(self, tree):
+        name = self.index_name(str(tree.get('name')))
+        self.push(name)
+        self.push('=')
+        self.visit(tree.expr)
+
+    def acceptAssignment(self, tree):
+        self.visit(tree.left)
+        self.push('=')
+        self.visit(tree.right)
+
+    # a,b = c [#MultiAssignment left: [# [#Name 'a'][#Name 'b']]right: [#Name 'c']]
+    def acceptMultiAssignment(self, tree):
+        self.acceptSeq(tree.left)
+        self.push('=')
+        if len(tree.right) == 0:
+            self.visit(tree.right)
+        else:
+            self.acceptSeq(tree.right)
+
+    # a += 1
+    def acceptSelfAssignment(self, tree):
+        self.visit(tree.left)
+        self.push(tree.name)
+        self.visit(tree.right)
+
+    def acceptDelete(self, tree):
+        self.push('del')
+        self.visit(tree.expr)
+
+    def acceptExpression(self, tree):
+        self.visit(tree[0])
+
+    # def f(a,b):
+    def acceptFuncDecl(self, tree):
+        self.push('def')
+        self.push(str(tree.name))
+        self.push('(')
+        self.acceptSeq(tree.params)
+        self.push(')')
+        self.visit(tree.body)
+
+    def acceptParamDecl(self, tree):
+        self.push(str(tree.name))
+
+    def acceptFuncExpr(self, tree):
+        self.push('lambda')
+        self.acceptSeq(tree.params)
+        self.push(':')
+        self.visit(tree.body)
+
+    # return
+
+    def acceptReturn(self, tree):
+        self.push('return')
+        if tree.has('expr'):
+            self.visit(tree.expr)
+
+    # Expression
+    def acceptName(self, tree):
+        name = self.index_name(str(tree))
+        self.push(name)
+
+    # [#ApplyExpr 'a']
+
+    def acceptApplyExpr(self, tree):
+        name = str(tree.name)
+        self.push(name)
+        self.push('(')
+        self.acceptSeq(tree.params)
+        self.push(')')
+
+    def acceptMethodExpr(self, tree):
+        self.visit(tree.recv)
+        self.push('.')
+        self.push(str(tree.name))
+        self.push('(')
+        self.acceptSeq(tree.params)
+        self.push(')')
+
+    #  o.name
+    def acceptGetExpr(self, tree):
+        self.visit(tree.recv)
+        self.push('.')
+        name = str(tree.name)
+        self.push(name)
+
+    def acceptIndexExpr(self, tree):
+        self.visit(tree.recv)
+        self.push('[')
+        self.visit(tree.index)
+        self.push(']')
+
+    def acceptSliceExpr(self, tree):
+        self.visit(tree.recv)
+        self.push('[')
+        if tree.has('start'):
+            self.visit(tree.start)
+        self.push(':')
+        if tree.has('end'):
+            self.visit(tree.end)
+        if tree.has('step'):
+            self.push(':')
+            self.visit(tree.step)
+        self.push(']')
+
+    def acceptUnary(self, tree):
+        name = str(tree.name)
+        self.push(name)
+        self.visit(tree.expr)
+
+    def acceptInfix(self, tree):
+        self.visit(tree.left)
+        name = str(tree.name)
+        self.push(name)
+        self.visit(tree.right)
+
+    def acceptMul(self, tree: ParseTree):
+        self.visit(tree.get('left', 0))
+        self.push('*')
+        self.visit(tree.get('right', 1))
+
+    def acceptAnd(self, tree):
+        self.visit(tree.left)
+        self.push('and')
+        self.visit(tree.right)
+
+    def acceptOr(self, tree):
+        self.visit(tree.left)
+        self.push('or')
+        self.visit(tree.right)
+
+    def acceptNot(self, tree):
+        self.push('not')
+        self.visit(tree.expr)
+
+    def acceptListArgument(self, tree):
+        self.push('*')
+        self.visit(tree[0])
+
+    def acceptGroup(self, tree):
+        self.push('(')
+        self.visit(tree[0])
+        self.push(')')
+
+    def acceptTuple(self, tree):
+        self.push('(')
+        for i, e in enumerate(tree.getSubNodes()):
+            if i > 0:
+                self.push(',')
+            self.visit(e)
+        self.push(')')
+
+    def acceptSet(self, tree):
+        self.push(f'({self.groupfy(tree)})の集合')
+
+    def acceptList(self, tree):
+        if len(tree) == 1 and tree[0] == 'ForExpr':
+            self.visit(tree[0])
+        else:
+            self.push('[')
+            self.acceptSeq(tree, ',')
+            self.push(']')
+
+    def acceptData(self, tree):
+        self.push('{')
+        self.acceptSeq(tree, ',')
+        self.push('}')
+
+    def acceptKeyValue(self, tree):
+        self.push(str(tree.name))
+        self.push(':')
+        self.visit(tree.value)
+
+    def acceptNull(self, tree):
+        self.push('None')
+
+    def acceptTrue(self, tree):
+        self.push('True')
+
+    def acceptFalse(self, tree):
+        self.push('False')
+
+    def acceptInt(self, tree):
+        self.push(str(tree))
+
+    def acceptFloat(self, tree):
+        self.push(str(tree))
+
+    def acceptDouble(self, tree):
+        self.push(str(tree))
+
+    def acceptQString(self, tree):
+        self.push(str(tree))
+
+    def acceptFormat(self, tree):
+        self.push(str(tree))
+
+    def acceptIfExpr(self, tree):
+        self.visit(tree.then)
+        self.push('if')
+        self.visit(tree.cond)
+        self.push('else')
+        self.visit(tree.get('else'))
+
+    def acceptForExpr(self, tree):
+        self.push('[')
+        self.visit(tree.append)
+        self.push('for')
+        self.acceptSeq(tree.each)
+        self.push('in')
+        self.visit(tree.list)
+        if tree.has('cond'):
+            self.push('if')
+            self.visit(tree.cond)
+        self.push(']')
+
+
+if __name__ == '__main__':
+    transpiler = PythonCode()
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as f:
+            compiled = transpiler.compile(f.read())
+            print(compiled)
---
+++ kolab/kotonoha/visitor.py
@@ -0,0 +1,596 @@
+import sys
+import csv
+import pathlib
+import pegtree as pg
+from pegtree import ParseTree
+
+from logging import getLogger
+logger = getLogger(__name__)
+
+
+class Visitor(object):
+    def __init__(self, methods=None):
+        self.methods = methods if methods is not None else {}
+
+    def visit(self, tree: ParseTree):
+        tag = tree.getTag()
+        methods = self.methods
+        if tag not in methods:
+            methods[tag] = f'accept{tag}'
+        method = methods[tag]
+        if hasattr(self, method):
+            method = getattr(self, method)
+            return method(tree)
+        else:
+            logger.warning(
+                f'@TODO undefined {method} method for {repr(tree)}')
+            self.push(str(tree))
+
+    def TODO(self, msg, tree):
+        logger.warning(f'@TODO {msg} for {repr(tree)}')
+
+
+PARENT = '..'
+
+
+class TransCompiler(Visitor):
+    buffers: list
+    env: dict
+    indent: int
+    level: int
+
+    def __init__(self, methods=None):
+        Visitor.__init__(self, methods)
+        self.buffers = []
+        self.env = {}
+        self.indent = 0
+
+    def push(self, *ss):
+        for s in ss:
+            s = str(s)
+            if s != '':
+                self.buffers.append(s)
+
+    def pushBOS(self, bos=''):
+        if len(self.buffers) > 0:
+            self.buffers.append('\n')
+        if self.indent > 0:
+            self.buffers.append("  " * self.indent)
+        self.push(bos)
+
+    def pushEOS(self, eos=''):
+        self.push(eos)
+
+    def getenv(self, key, default_value={}):
+        env = self.env
+        while env is not None:
+            if key in env:
+                return env[key]
+            env = env.get(PARENT, None)
+        if '.' in key:
+            key = key[key.find('.')+1:]
+            return self.getenv(key, default_value)
+        return default_value
+
+    def hasenv(self, key):
+        return self.getenv(self, key) is not None
+
+    def setenv(self, key, value):
+        self.env[key] = value
+
+    def stringfy(self, tree, sep=''):
+        stacked = self.buffers
+        self.buffers = []
+        self.visit(tree)
+        s = sep.join(self.buffers)
+        self.buffers = stacked
+        return s
+
+
+# 文字列操作
+
+
+def chunk(s):
+    nested = 0
+    found = False
+    for i in range(len(s)):
+        if s.startswith("{{", i):
+            nested += 1
+        elif s.startswith("}}", i):
+            nested -= 1
+        elif nested == 0 and s[i] in "にをがで":
+            found = True
+            break
+        else:
+            pass
+    if found:
+        return '{{' + s + '}}'
+    return s
+
+
+THEN = {
+    'く': 'き', 'す': 'し', 'つ': 'ち', 'ぬ': 'に', 'む': 'み',
+    'う': 'い', 'ぐ': 'ぎ', 'ぶ': 'び', 'た': 'て', 'だ': 'で'
+}
+
+
+def and_then(s):
+    if s.endswith('かどうか'):
+        s = s[:-4]
+    if s.endswith('い'):   # 美しい ない
+        return s[:-1] + 'く'
+    if s.endswith('する'):  # 含まれる  高め
+        return s[:-2] + 'して'
+    if s.endswith('る'):  # 含まれる  高め
+        return s[:-1]
+    if s[-1] in THEN:
+        return s[:-1] + THEN[s[-1]]
+    return s
+
+
+NAI = {
+    'く': 'か', 'す': 'さ', 'つ': 'た', 'ぬ': 'な', 'む': 'ま',
+    'う': 'わ', 'ぐ': 'が', 'ぶ': 'ば', 'た': 'て', 'だ': 'で'
+}
+
+
+def not_nai(s):  # ないは自分でつける
+    if s.endswith('かどうか'):
+        s = s[:-4]
+    if s.endswith('い'):   # 美しい ない
+        return s[:-1] + 'く'
+    if s.endswith('する'):  # 使用する
+        return s[:-2] + 'し'
+    if s.endswith('る'):  # 含まれる  高め
+        return s[:-1]
+    if s[-1] in NAI:
+        return s[:-1] + NAI[s[-1]]
+    return s + 'で'
+
+
+def and_noun(s):  # +とき
+    if s.endswith('かどうか'):
+        s = s[:-4]
+        # 大きい　含まれる　含まれた
+        if s[-1] in "いるた":
+            return s
+    if s[-1] in 'いるた':  # する
+        return s
+    if s[-1] in NAI:
+        return s
+    return s + 'の'
+
+
+# PJ
+
+base = pathlib.Path(__file__).parent.resolve()
+
+PARAMS = ('{}', '{0}', '{1}', '{2}', '{3}', '{4}', '{5}')
+
+
+def check_pair(key, value, env):
+    if 'A' in value:
+        value = value.replace('A', '{0}')
+        value = value.replace('B', '{1}')
+        value = value.replace('C', '{2}')
+        value = value.replace('D', '{3}')
+        value = value.replace('E', '{4}')
+        value = value.replace('F', '{5}')
+
+    localkey = sum(value.count(x) for x in PARAMS)
+    if '@' in key:
+        key, localkey = key.split('@')
+    if key not in env:
+        env[key] = {}
+    entry = env[key]
+    if localkey in entry and entry[localkey] == value:
+        logger.warning(f'duplicated key: {key}@{localkey}')
+    entry[localkey] = value
+    #print(value, and_then(value), and_noun(value)+'とき', not_nai(value))
+
+
+def read_corpus(module, env={}, experimental=False):
+    file = module
+    if not module.endswith('.csv'):
+        if '/' in module:
+            file = f'{base}/{module}.csv'
+        else:
+            file = f'{base}/python-corpus/{module}.csv'
+    with open(file) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if len(row) >= 2 and row[1] != '':
+                if experimental and len(row) >= 3:
+                    check_pair(row[0], row[2], env)
+                else:
+                    check_pair(row[0], row[1], env)
+
+
+PYTHON = {}
+
+TYPES = {
+    'List': 'list', 'Set': 'set', 'Int': 'int', 'QString': 'str',
+    'True': 'bool', 'False': 'bool', 'Float': 'float', 'Double': 'float',
+}
+
+
+def guess_type(tree, default_type=''):
+    tag = tree.getTag()
+    if tag in TYPES:
+        return TYPES[tag]
+    return default_type
+
+
+py = 'py'
+ja = 'ja'
+
+
+class Kotonoha(Visitor):
+    parser: object
+
+    def __init__(self, grammar='../pegtree/puppy2.tpeg'):
+        peg = pg.grammar(grammar)
+        self.parser = pg.generate(peg)
+        self.rootEnv = {}
+
+    def load(self, modules, experimental=False):
+        for module in modules.split(':'):
+            read_corpus(module, self.rootEnv, experimental)
+
+    def compile(self, source):
+        tree = self.parser(source)
+        self.buffers = []
+        if len(self.rootEnv) == 0:
+            self.load('python3:builtin:random')
+        self.env = {PARENT: self.rootEnv}
+        self.indent = 0
+        self.level = 0
+        self.visit(tree)
+        return ''.join(self.buffers)
+
+    # Source
+
+    def acceptSource(self, tree):
+        trees = tree.getSubNodes()
+        for t in trees[:-1]:
+            self.visit(t)
+        self.visit(trees[-1])
+
+    def acceptBlock(self, tree):
+        self.indent += 1
+        self.acceptSource(tree)
+        self.indent -= 1
+
+    # Statement
+
+    def acceptPass(self, tree):
+        self.pushSentence('pass')
+        self.pushStatement('pass')
+
+    # if a > 0: pass
+    def acceptAssert(self, tree):
+        self.pushSentence('assert', self.stringfy(tree.cond, trf=not_nai))
+        self.pushStatement('assert', tree.cond)
+
+    # if a > 0: pass
+    def acceptIf(self, tree):
+        self.pushSentence('if', self.stringfy(tree.cond, trf=and_noun))
+        self.pushStatement('if', tree.cond)
+        self.visit(tree.get('then'))
+        if tree.has('elif'):
+            for t in tree.get('elif').getSubNodes():
+                self.visit(t)
+        if tree.has('else'):
+            self.pushSentence('else')
+            self.pushStatement('else')
+            self.visit(tree.get('else'))
+
+    def acceptElif(self, tree, ):
+        self.pushSentence('elif', self.stringfy(tree.cond, trf=and_noun))
+        self.pushStatement('elif', tree.cond)
+        self.visit(tree.get('then'))
+
+    def acceptWhile(self, tree):
+        self.pushSentence('while', self.stringfy(tree.cond, trf=and_noun))
+        self.pushStatement('while', tree.cond)
+        self.visit(tree.body)
+
+    # break
+    def acceptBreak(self, tree):
+        self.pushSentence('break')
+        self.pushStatement('break')
+
+    # continue
+    def acceptContinue(self, tree):
+        self.pushSentence('continue')
+        self.pushStatement('continue')
+
+    def acceptFor(self, tree):
+        self.pushSentence('for', self.groupfy(tree.each), tree.list)
+        self.pushStatement('for', tree.each, tree.list)
+        self.visit(tree.body)
+
+    def acceptImportDecl(self, tree):
+        name = str(tree.name)
+        self.pushSentence('import', name)
+        self.pushStatement('import', name)
+
+    def acceptVarDecl(self, tree):
+        #name = str(tree.get('name', 'left'))
+        self.pushParallelCorpus(tree)
+        name = str(tree.get('name'))
+        self.pushSentence('=', name, tree.expr)
+        self.pushStatement('=', name, tree.expr)
+
+    def acceptAssignment(self, tree):
+        #name = str(tree.get('name', 'left'))
+        name = str(tree.get('left'))
+        self.pushSentence('=A', name, tree.right)
+        self.pushStatement('=A', name, tree.right)
+
+    # a,b = c [#MultiAssignment left: [# [#Name 'a'][#Name 'b']]right: [#Name 'c']]
+    def acceptMultiAssignment(self, tree):
+        self.pushParallelCorpus(tree)
+        if len(tree.left) == 2 and len(tree.right) == 2:
+            A = str(tree.left[0])
+            B = str(tree.left[1])
+            AA = str(tree.right[0])
+            BB = str(tree.right[1])
+            if A == BB and B == AA:
+                self.pushSentence('=SWAP', tree.left[0], tree.left[1])
+                self.pushStatement('=SWAP', tree.left[0], tree.left[1])
+                return
+        #print('@', repr(tree.right), len(tree.right))
+        if tree.right == 'Tuple':
+            self.pushSentence('=', self.groupfy(tree.left),
+                              self.groupfy(tree.right))
+            self.pushStatement('=', tree.left, tree.right)
+        else:
+            self.pushSentence('=D', self.groupfy(tree.left), tree.right)
+            self.pushStatement('=D', tree.left, tree.right)
+
+    # a += 1
+    def acceptSelfAssignment(self, tree):
+        self.pushParallelCorpus(tree)
+        name = str(tree.name)
+        self.pushSentence(name, [tree.left, tree.right])
+        self.pushStatement(name, [tree.left, tree.right])
+
+    def acceptDelete(self, tree):
+        self.pushParallelCorpus(tree)
+        self.pushSentence('del', tree.expr)
+        self.pushStatement('del', tree.expr)
+
+    def acceptExpression(self, tree):
+        self.pushParallelCorpus(tree)
+        self.pushSentence('', tree[0])
+        self.pushStatement('', tree[0])
+
+    # def f(a,b):
+    def acceptFuncDecl(self, tree):
+        name = str(tree.name)
+        self.pushSentence('def', name, tree.params)
+        self.pushStatement('def', name, tree.params)
+        stacked_env = self.env
+        self.env = {PARENT: self.env}
+        self.visit(tree.body)
+        self.env = stacked_env
+
+    # return
+    def acceptReturn(self, tree):
+        self.pushParallelCorpus(tree)
+        if tree.has('expr'):
+            self.pushSentence('return', tree.expr)
+            self.pushStatement('return', tree.expr)
+        else:
+            self.pushSentence('return')
+            self.pushStatement('return', '')
+
+    # Expression
+
+    def acceptName(self, tree):
+        name = str(tree)
+        defined = self.getenv(name)
+        if 0 in defined:
+            self.push(defined[0])
+        else:
+            self.push(str(tree))
+
+    # [#ApplyExpr 'a']
+
+    def acceptApplyExpr(self, tree):
+        name = str(tree.name)
+        defined = self.getenv(name)
+        params, extra = self.makeParamList(tree.params, defined)
+        self.pushApplication(name, params, defined, extra)
+
+    def makeParamList(self, params, fd):
+        p = []
+        p2 = []
+        for tree in params.getSubNodes():
+            #print(repr(tree), f'{tree[0].name}' if len(tree) > 0 else '@')
+            if tree.getTag() == 'Data' and len(tree) > 0 and f'{tree[0].name}=' in fd:
+                for t in tree.getSubNodes():
+                    key = str(t.name)+'='
+                    keyval = key+str(t.value).replace("'", '"')
+                    if keyval in fd:
+                        p2.append(fd[keyval])
+                    elif key in fd:
+                        p2.append(fd[key].format(self.stringfy(t.value)))
+            else:
+                p.append(self.stringfy(tree))
+        return p, ''.join(p2)
+
+    def acceptMethodExpr(self, tree):
+        #print('@recv', recv)
+        name = str(tree.name)
+        defined = self.getenv(f'{str(tree.recv)}.{name}')
+        if len(defined) > 0:
+            params, extra = self.makeParamList(tree.params, defined)
+            self.pushApplication(name, params, defined, extra)
+        else:
+            recv = self.stringfy(tree.recv)
+            defined = self.getenv(f'.{name}')
+            if len(defined) > 0:
+                params, extra = self.makeParamList(tree.params, defined)
+                params = [recv] + params
+                self.pushApplication(name, params, defined, extra)
+            else:
+                self.push(str(tree))
+
+    #  o.name
+    def acceptGetExpr(self, tree):
+        recv = self.stringfy(tree.recv)
+        name = self.stringfy(tree.name)
+        defined = self.getenv(f'.{name}')
+        if len(defined) > 0:
+            self.push(f'{recv}の{defined.get(0, name)}')
+            return
+        defined = self.getenv(name)
+        if len(defined) > 0:
+            self.push(f'{recv}の{defined.get(0, name)}')
+        else:
+            self.push(f'{recv}.{name}')
+
+    def acceptIndexExpr(self, tree):
+        suffix = str(tree)
+        suffix = suffix[suffix.rfind('['):]
+        defined = self.getenv(suffix+'C')
+        if len(defined) > 0 and 1 in defined:
+            self.pushApplication(suffix, [tree.recv], defined)
+            return
+        self.pushApplication('[]', [tree.recv, tree.index])
+
+    def acceptSliceExpr(self, tree):
+        suffix = str(tree)
+        suffix = suffix[suffix.rfind('['):]
+        defined = self.getenv(suffix+'C')
+        if len(defined) > 0 and 1 in defined:
+            self.pushApplication(suffix, [tree.recv], defined)
+            return
+        if not tree.has('end') and not tree.has('step'):
+            if tree.has('start'):
+                self.pushApplication('[:]', [tree.recv, tree.start])
+            else:
+                self.pushApplication('[:]', [tree.recv])
+            return
+        params = [tree.recv]
+        if tree.has('start'):
+            params.append(tree.start)
+        else:
+            params.append('先頭')
+        if tree.has('end'):
+            params.append(tree.end)
+        else:
+            params.append('末尾')
+        if tree.has('step'):
+            params.append(tree.step)
+        self.pushApplication('[::]', params)
+
+    def acceptUnary(self, tree):
+        name = str(tree.name)
+        p = [self.stringfy(tree.expr)]
+        self.pushApplication(name, p, self.getenv(name))
+
+    def acceptInfix(self, tree):
+        name = str(tree.name)
+        p = [self.stringfy(tree.left), self.stringfy(tree.right)]
+        self.pushApplication(name, p, self.getenv(name))
+
+    def acceptMul(self, tree):
+        if tree.left == 'List':
+            self.pushApplication('list.*', [tree.left, tree.right])
+        else:
+            self.pushApplication('list.*', [tree.left, tree.right])
+
+    def acceptAnd(self, tree):
+        p = [self.stringfy(tree.left, trf=and_then), self.stringfy(tree.right)]
+        self.push(f'{p[0]}、かつ{p[1]}')
+
+    def acceptOr(self, tree):
+        p = [self.stringfy(tree.left, trf=and_then), self.stringfy(tree.right)]
+        self.push(f'{p[0]}、または{p[1]}')
+
+    def acceptNot(self, tree):
+        self.push(self.stringfy(tree.expr, trf=not_nai) + 'ない')
+
+    def acceptListArgument(self, tree):
+        self.pushApplication('*', [tree[0]], self.getenv('*'))
+
+    def acceptGroup(self, tree):
+        self.visit(tree[0])
+
+    def acceptTuple(self, tree):
+        self.push(f'({self.groupfy(tree)})の組')
+
+    def acceptSet(self, tree):
+        self.push(f'({self.groupfy(tree)})の集合')
+
+    def symbol(self, key, param=0):
+        entry = self.getenv(key)
+        if param == 0 or param == None:
+            return entry[0]
+        else:
+            return entry[1].format(param)
+
+    def acceptList(self, tree):
+        if len(tree) == 1 and tree[0] == 'ForExpr':
+            self.push(self.stringfy(tree[0]))
+        else:
+            self.push(self.symbol('[]', None if len(
+                tree) == 0 else self.groupfy(tree)))
+
+    def acceptData(self, tree):
+        self.push(self.symbol('{}', None if len(
+            tree) == 0 else self.groupfy(tree)))
+
+    def acceptKeyValue(self, tree):
+        key = self.stringfy(tree.name)
+        value = self.stringfy(tree.value)
+        self.push(f'({key}, {value})')
+
+    def acceptNull(self, tree):
+        self.push(self.symbol('None'))
+
+    def acceptTrue(self, tree):
+        self.push(self.symbol('True'))
+
+    def acceptFalse(self, tree):
+        self.push(self.symbol('False'))
+
+    def acceptInt(self, tree):
+        self.push(str(tree))
+
+    def acceptFloat(self, tree):
+        self.push(str(tree))
+
+    def acceptDouble(self, tree):
+        self.push(str(tree))
+
+    def acceptQString(self, tree):
+        self.push(str(tree))
+
+    def acceptIfExpr(self, tree):
+        defined = self.getenv('if')
+        cond = self.stringfy(tree.cond, trf=and_noun)
+        self.pushApplication(
+            'if', [cond, tree.then, tree.get('else')], defined)
+
+    def acceptForExpr(self, tree):
+        defined = self.getenv('for')
+        vars = self.groupfy(tree.each)
+        if tree.has('cond'):
+            self.pushApplication('for', [tree.append, vars, tree.list, self.stringfy(
+                tree.cond, trf=and_noun)], defined)
+        else:
+            self.pushApplication(
+                'for', [tree.append, vars, tree.list], defined)
+
+
+if __name__ == '__main__':
+    transpiler = Kotonoha()
+    transpiler.load('python3:builtin:random')
+    # print(transpiler.compile("if defined:"))
+    if len(sys.argv) > 1:
+        with open(sys.argv[1]) as f:
+            print(transpiler.compile(f.read()))

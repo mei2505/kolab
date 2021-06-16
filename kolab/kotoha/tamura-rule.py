e29363e63ea7112a8bc22fd31110a096f0f60e4c
mei2505 <baron.melon.1109@gmail.com>
2021-06-16 09:50:25 +0900

    example

---
+++ kolab/kotoha/tamura-rule.py
@@ -0,0 +1,19 @@
+'''
+# 例
+x,y,z = ''
+list, list2 = 'リスト'
+
+# カタカナ英語を挿入する
+_アペンドする = '追加する|アペンドする'
+_エクステンドする = '追加する|エクステンドする'
+
+# 動詞で終わらせる
+list.append(x) # listにxをアペンドする
+list.extend(list2) # listの最後尾にlist2をエクステンドする
+
+list.myfind(x) # listからx(要素)を探す -> int(位置)
+
+'''
+
+import pandas as pd
+
